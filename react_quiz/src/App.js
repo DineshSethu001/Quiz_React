15e6b1a45bb0a7e6306bc9715869b32a6d682022
DineshSethu001 <dineshsethu15981@gmail.com>
2023-12-27 06:33:25 +0530

    checking pending

---
+++ react_quiz/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Quiz_app from './components/Quiz_app'
+import './index.css'
+
+export default function App(){
+  return(
+    <>
+    <Quiz_app />
+    </>
+  )
+}
